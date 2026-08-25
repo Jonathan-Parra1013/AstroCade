b47d52c4835025cef7b3c1e715a01749152bb522
JDMvides <jggameryt321@gmail.com>
2026-08-24 20:12:30 -0500

    first commit test

---
+++ src/main/java/com/github/jdmvides/astrocade/Astrocade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.jdmvides.astrocade;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Astrocade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
