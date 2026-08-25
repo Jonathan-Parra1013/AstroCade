030951cb844a09feee76fc036857efaefa25b0a2
Nicoxso <nicolas-solartem@unilibre.edu.co>
2026-08-24 21:06:47 -0500

    Update MaquinaArcade.java

--- src/MaquinaArcade.java
+++ src/MaquinaArcade.java
@@ -1,2 +1,24 @@
-system.println
-  
+public class MaquinaArcade {
+
+    private String nombre;
+    private double precioPartida;
+
+    public MaquinaArcade(String nombre, double precioPartida) {
+        this.nombre = nombre;
+        this.precioPartida = precioPartida;
+    }
+
+    public void iniciarPartida() {
+        if (saldo >= precioPartida) {
+            saldo -= precioPartida;
+            System.out.println("Partida iniciada en " + nombre);
+        } else {
+            System.out.println("No hay suficiente crédito.");
+        }
+    }
+
+    public void mostrarInformacionMaquina() {
+        System.out.println("Máquina: " + nombre);
+        System.out.println("Precio por partida: $" + precioPartida);
+    }
+}
